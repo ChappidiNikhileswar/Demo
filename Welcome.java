ad8474a72458ad344d2eab256d2bd75b45aaa18e
ChappidiNikhileswar <33716187+ChappidiNikhileswar@users.noreply.github.com>
2017-11-21 12:18:37 +0530

    Updated code

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,9 @@
 public class Welcome{
   static int i;
-public static void main(String[] args){
-for(i=0;i<10;i++){
-System.out.println("Hii Nikhil");
-}
-System.out.println("Welcome");
+  public static void main(String[] args){
+    for(i=0;i<10;i++){
+    System.out.println("Hii Nikhil");
+    }
+    System.out.println("Welcome");
+  }
 }
